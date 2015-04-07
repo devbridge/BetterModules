5cda696f4decd91e58c884256e015035682f6c3e
Mantas Janulionis <mantas.janulionis@devbridge.com>
2015-04-07 10:55:00 +0300

    Added installation script for FluentMigrator.Runner.dll

---
+++ BetterModules.NugetPackage/Packages/BetterModules.Core/install.ps1
@@ -0,0 +1,5 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+# Add reference.
+$project.Object.References | Where-Object { $_.Name -eq 'FluentMigrator.Runner' } | ForEach-Object { $_.Remove() }
+$project.Object.References.Add("$installPath\..\FluentMigrator.Tools.1.0.6.0\tools\AnyCPU\40\FluentMigrator.Runner.dll")
