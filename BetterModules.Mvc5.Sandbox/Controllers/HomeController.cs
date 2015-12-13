d529c13ad281616f769eb730ae1c49d89b32694b
Paulius Maciulis <paulius@devbridge.com>
2015-12-14 01:46:24 +0200

    nuspec updated. RazorGenerator.Mvc.dll lib removed and added as nuget package.

--- BetterModules.Mvc5.Sandbox/Controllers/HomeController.cs
+++ BetterModules.Mvc5.Sandbox/Controllers/HomeController.cs
@@ -15,31 +15,6 @@ namespace BetterModules.Mvc5.Sandbox.Controllers
     {
         public ActionResult Index()
         {
-            using (var scope = ContextScopeProvider.CreateChildContainer())
-            {
-                string postfix = "__1";
-
-                TestItemCategory category = new TestItemCategory();
-                category.Name = "test_pm" + postfix;
-
-                TestItemModel item = new TestItemModel();
-                item.Name = "big test" + postfix;
-                item.Category = category;
-
-                IRepository repository = scope.Resolve<IRepository>();
-                IUnitOfWork unitOfWork = scope.Resolve<IUnitOfWork>();
-
-                repository.Save(item);
-                unitOfWork.Commit();
-
-                item.Name = "ups";
-                item.Version = 3;
-                repository.Save(item);
-                unitOfWork.Commit();
-
-                var l = repository.AsQueryable<TestItemCategory>().ToList();
-            }
-
             return View();
         }
     }
