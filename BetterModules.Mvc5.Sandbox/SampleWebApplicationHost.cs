e2a87a9e1e8f57ff0cebe816b8b7d4d251c70869
Mantas Janulionis <mantas.janulionis@devbridge.com>
2016-05-05 11:00:33 +0300

    Removed auto start due to test purposes

--- BetterModules.Mvc5.Sandbox/SampleWebApplicationHost.cs
+++ BetterModules.Mvc5.Sandbox/SampleWebApplicationHost.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Web;
+
+using BetterModules.Core.DataAccess.DataContext.Migrations;
 using BetterModules.Core.Exceptions;
 using BetterModules.Core.Web.Environment.Application;
 using BetterModules.Core.Web.Environment.Host;
 using BetterModules.Core.Web.Modules.Registration;
 using BetterModules.Mvc5.Sandbox;
 
-[assembly : WebApplicationHost(typeof(SampleDefaultWebApplicationAutoHost), Order = 100)]
+[assembly : WebApplicationHost(typeof(SampleWebApplicationHost), Order = 100)]
 namespace BetterModules.Mvc5.Sandbox
 {
-    public class SampleDefaultWebApplicationAutoHost : DefaultWebApplicationAutoHost
+    public class SampleWebApplicationHost : DefaultWebApplicationHost
     {
+        public SampleWebApplicationHost(IWebModulesRegistration modulesRegistration, IMigrationRunner migrationRunner)
+            : base(modulesRegistration, migrationRunner)
+        {
+        }
+
         HttpApplication SenderAsHttpApplication(object sender)
         {
             var application = sender as HttpApplication;
